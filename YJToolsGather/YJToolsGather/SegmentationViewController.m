706ee24cbf6ae9fc8b41348d488c3a2372aa5377
yuejun.hou <yuejun.hou@rograndec.com>
2016-12-21 11:38:17 +0800

    5522222

--- YJToolsGather/YJToolsGather/SegmentationViewController.m
+++ YJToolsGather/YJToolsGather/SegmentationViewController.m
@@ -25,6 +25,37 @@
         NSLog(@"--->>>>> %@", obj);
     }];
 
+    [self CAGradientLayer];
+}
+
+- (void)CAGradientLayer{
+    
+    CGRect frame = CGRectMake(0, 64, kScreen_Width, 40);
+    UILabel *labelInit = [[UILabel alloc] initWithFrame:frame];
+    labelInit.font = [UIFont systemFontOfSize:13];
+    labelInit.textAlignment = NSTextAlignmentLeft;
+    labelInit.backgroundColor = [UIColor grayColor];
+    labelInit.numberOfLines = 0;
+    labelInit.text = @"太阳对我眨眼睛鸟儿唱歌给我听我是一个努力干活儿还不粘人的小妖精别问我从哪里来也别问我到哪里去我要摘下最美的花儿献给我的小公举大王叫我来巡山我把人间转一转打起我的鼓敲起我的锣";
+    [self.view addSubview:labelInit];
+    
+    NSArray *colors = [NSArray arrayWithObjects:
+                       (id)[[UIColor colorWithWhite:0 alpha:1] CGColor],
+                       (id)[[UIColor colorWithWhite:0 alpha:0] CGColor],
+                       nil];
+    
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    [gradientLayer setColors:colors];
+    [gradientLayer setStartPoint:CGPointMake(0.5f, 1.0f)]; // 单位向量
+    [gradientLayer setEndPoint:CGPointMake(0.5f, 0.0f)];
+    [gradientLayer setFrame:[labelInit bounds]];
+    gradientLayer.type = kCAGradientLayerAxial;
+    
+    [[labelInit layer] setMask:gradientLayer];
+
+    
+
+
 }
 
 
